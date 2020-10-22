cc265ce1d8d5710f8de826b870f86a68c1213a7e
kovacsgaborab <kovacsgaborab@gmail.com>
2020-10-22 09:28:09 +0200

    Add project files.

---
+++ Telekocsi_Form/Auto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telekocsi_Form
+{
+    class Auto
+    {
+        public string Indulas { get; set; }
+        public string Cel { get; set; }
+        public string Rendszam { get; set; }
+        public string Telefonszam { get; set; }
+        public int Ferohely { get; set; }
+        public string Utvonal { get; private set; }
+
+
+        public Auto(string sor)
+        {
+            string[] a = sor.Split(';');
+            Indulas = a[0];
+            Cel = a[1];
+            Rendszam = a[2];
+            Telefonszam = a[3];
+            Ferohely = Convert.ToInt32(a[4]);
+            Utvonal = Indulas + "-" + Cel;
+        }
+    }
+}
---
+++ Telekocsi_Form/Igeny.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telekocsi_Form
+{
+    class Igeny
+    {
+        public string Azonosito { get; set; }
+        public string Indulas { get; set; }
+        public string Cel { get; set; }
+        public int Szemelyek { get; set; }
+        public string Utvonal { get; set; }
+
+
+        public Igeny(string sor)
+        {
+            string[] a = sor.Split(';');
+            Azonosito = a[0];
+            Indulas = a[1];
+            Cel = a[2];
+            Szemelyek = Convert.ToInt32(a[3]);
+            Utvonal = Indulas + "-" + Cel;
+        }
+
+
+        public int VanAuto(List<Auto> autok)
+        {
+            int i = 0;
+            while (i < autok.Count && !(Utvonal == autok[i].Utvonal && Szemelyek <= autok[i].Ferohely))
+            {
+                i++;
+            }
+            if (i < autok.Count)
+            {
+                return i;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+}
